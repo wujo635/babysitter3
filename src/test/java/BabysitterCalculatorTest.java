5f973bdaf951a6159a5c5a252861734c2c6767a4
wujo635 <wujo635@gmail.com>
2019-01-15 18:54:54 -0500

    Setup gradle and test framework and added failing sanity check test case

---
+++ src/test/java/BabysitterCalculatorTest.java
@@ -0,0 +1,11 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class BabysitterCalculatorTest {
+
+    @Test
+    public void sanityCheck() {
+        assertEquals(1,2);
+    }
+
+}
