9c944475c3e50815c9608a8130794b0ba2280ce5
wujo635 <wujo635@gmail.com>
2019-01-16 00:42:35 -0500

    Passing test for start time before 5pm

--- src/main/java/kata/BabysitterCalculator.java
+++ src/main/java/kata/BabysitterCalculator.java
@@ -2,6 +2,9 @@ package kata;
 
 public class BabysitterCalculator {
     public int calculate(int start, int end) {
+        if (start < 17) {
+            return -1;
+        }
         return (end - start);
     }
 }
