6fb5bb618c8ff58512473a0f73e1f40a7f0e95d9
wujo635 <wujo635@gmail.com>
2019-01-17 17:46:08 -0500

    Passing test for end time out of bounds at 3pm

--- src/main/java/kata/BabysitterCalculator.java
+++ src/main/java/kata/BabysitterCalculator.java
@@ -5,7 +5,7 @@ public class BabysitterCalculator {
         if (start < 17 && start > 4) {
             return -1;
         }
-        if (end == 5) {
+        if (end < 17 && end > 4) {
             return -1;
         }
         return (end - start);
