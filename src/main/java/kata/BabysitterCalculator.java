898dd790b2bb93dd15b428ccc996fc43fce3fe4c
wujo635 <wujo635@gmailc.om>
2019-01-20 10:05:18 -0500

    Refactor check for invalid working hours to separate method

--- src/main/java/kata/BabysitterCalculator.java
+++ src/main/java/kata/BabysitterCalculator.java
@@ -2,10 +2,14 @@ package kata;
 
 public class BabysitterCalculator {
     public int calculate(int start, int end) {
-        if (isNotWithinWorkingHours(start) || isNotWithinWorkingHours(end) || isEndBeforeStartTime(start, end)) {
+        if (isInvalidWorkingHours(start, end)) {
             return -1;
         }
         return (end - start);
+    }
+
+    private boolean isInvalidWorkingHours(int start, int end) {
+        return isNotWithinWorkingHours(start) || isNotWithinWorkingHours(end) || isEndBeforeStartTime(start, end);
     }
 
     private boolean isEndBeforeStartTime(int start, int end) {
