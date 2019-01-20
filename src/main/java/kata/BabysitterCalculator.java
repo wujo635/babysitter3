5249263aef1440899f07616bfe1c8aefe9ba050a
wujo635 <wujo635@gmailc.om>
2019-01-20 00:17:46 -0500

    Refactor checks for invalid start and end times into single conditional

--- src/main/java/kata/BabysitterCalculator.java
+++ src/main/java/kata/BabysitterCalculator.java
@@ -2,13 +2,14 @@ package kata;
 
 public class BabysitterCalculator {
     public int calculate(int start, int end) {
-        if (isNotWithinWorkingHours(start) || isNotWithinWorkingHours(end)) {
-            return -1;
-        }
-        if (end < start) {
+        if (isNotWithinWorkingHours(start) || isNotWithinWorkingHours(end) || isEndBeforeStartTime(start, end)) {
             return -1;
         }
         return (end - start);
+    }
+
+    private boolean isEndBeforeStartTime(int start, int end) {
+        return end < start;
     }
 
     private boolean isNotWithinWorkingHours(int time) {
