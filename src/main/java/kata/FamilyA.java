12972547277ec6f28ce711b8205f9f67176011b7
wujo635 <wujo635@gmailc.om>
2019-01-27 15:39:18 -0500

    Cleaned out hour payrates

--- src/main/java/kata/FamilyA.java
+++ src/main/java/kata/FamilyA.java
@@ -7,17 +7,17 @@ public class FamilyA implements Family {
     @Override
     public Map<Integer, Integer> getHourPayMap() {
         HashMap<Integer,Integer> hourPayMap = new HashMap<>();
-        hourPayMap.put(0, 20);
-        hourPayMap.put(1, 20);
-        hourPayMap.put(2, 20);
-        hourPayMap.put(3, 20);
-        hourPayMap.put(17, 15);
-        hourPayMap.put(18, 15);
-        hourPayMap.put(19, 15);
-        hourPayMap.put(20, 15);
-        hourPayMap.put(21, 15);
-        hourPayMap.put(22, 15);
-        hourPayMap.put(23, 20);
+//        hourPayMap.put(0, 20);
+//        hourPayMap.put(1, 20);
+//        hourPayMap.put(2, 20);
+//        hourPayMap.put(3, 20);
+//        hourPayMap.put(17, 15);
+//        hourPayMap.put(18, 15);
+//        hourPayMap.put(19, 15);
+//        hourPayMap.put(20, 15);
+//        hourPayMap.put(21, 15);
+//        hourPayMap.put(22, 15);
+//        hourPayMap.put(23, 20);
         return hourPayMap;
     }
 }
--- src/main/java/kata/FamilyB.java
+++ src/main/java/kata/FamilyB.java
@@ -8,17 +8,17 @@ public class FamilyB implements Family {
     @Override
     public Map<Integer, Integer> getHourPayMap() {
         HashMap<Integer,Integer> hourPayMap = new HashMap<>();
-        hourPayMap.put(0, 16);
-        hourPayMap.put(1, 16);
-        hourPayMap.put(2, 16);
-        hourPayMap.put(3, 16);
-        hourPayMap.put(17, 12);
-        hourPayMap.put(18, 12);
-        hourPayMap.put(19, 12);
-        hourPayMap.put(20, 12);
-        hourPayMap.put(21, 12);
-        hourPayMap.put(22, 8);
-        hourPayMap.put(23, 8);
+//        hourPayMap.put(0, 16);
+//        hourPayMap.put(1, 16);
+//        hourPayMap.put(2, 16);
+//        hourPayMap.put(3, 16);
+//        hourPayMap.put(17, 12);
+//        hourPayMap.put(18, 12);
+//        hourPayMap.put(19, 12);
+//        hourPayMap.put(20, 12);
+//        hourPayMap.put(21, 12);
+//        hourPayMap.put(22, 8);
+//        hourPayMap.put(23, 8);
         return hourPayMap;
     }
 
